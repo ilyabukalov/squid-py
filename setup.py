fc6700cf0018047024a73692d50a0789af821316
Javier Cortejoso <javier.cortejoso@gmail.com>
2019-01-07 13:22:29 +0100

    Bumped keeper-contracts dependency to 0.5.2

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open('HISTORY.md') as history_file:
 install_requirements = [
     'coloredlogs',
     'eciespy',
-    'keeper-contracts==0.5.1',
+    'keeper-contracts==0.5.2',
     'pyopenssl',
     'PyJWT',  # not jwt
     'PyYAML',
