1bb96545a0cda13b14889e595dfb17ed0ce6b6e1
eruizgar91 <enrique@oceanprotocol.com>
2019-01-18 16:37:27 +0100

    Remove dids in services according the ddo example

--- squid_py/ddo/service.py
+++ squid_py/ddo/service.py
@@ -10,15 +10,14 @@ import re
 class Service:
     """Service class to create validate service in a DDO."""
 
-    def __init__(self, service_id, endpoint, service_type, values):
+    def __init__(self, endpoint, service_type, values):
         """Initialize Service instance."""
-        self._id = service_id
         self._endpoint = endpoint
         self._type = service_type
 
         # assign the _values property to empty until they are used
         self._values = {}
-        reserved_names = {'id', 'serviceEndpoint', 'type'}
+        reserved_names = {'serviceEndpoint', 'type'}
         if values:
             for name, value in values.items():
                 if name not in reserved_names:
@@ -27,15 +26,6 @@ class Service:
     @property
     def type(self):
         return self._type
-
-    def get_id(self):
-        """Return the service id."""
-        return self._id
-
-    def assign_did(self, did):
-        """ Assign a new DID/Id to the service."""
-        if re.match('^#.*', self._id):
-            self._id = did + self._id
 
     def get_type(self):
         """Get the service type."""
@@ -63,12 +53,11 @@ class Service:
     def as_text(self, is_pretty=False):
         """Return the service as a JSON string."""
         values = {
-            'id': self._id,
             'type': self._type,
             'serviceEndpoint': self._endpoint
         }
         if self._values:
-            # add extra service values to the dictonairy
+            # add extra service values to the dictionary
             for name, value in self._values.items():
                 values[name] = value
 
@@ -80,12 +69,11 @@ class Service:
     def as_dictionary(self):
         """Return the service as a python dictionary."""
         values = {
-            'id': self._id,
             'type': self._type,
             'serviceEndpoint': self._endpoint
         }
         if self._values:
-            # add extra service values to the dictonairy
+            # add extra service values to the dictionary
             for name, value in self._values.items():
                 if isinstance(value, object) and hasattr(value, 'as_dictionary'):
                     value = value.as_dictionary()
