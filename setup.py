dfffe038de71c067e0e70d55fdc105e562ca37f2
ssallam <samer@oceanprotocol.com>
2019-03-20 14:47:17 +0100

    Bump version: 0.5.3 → 0.5.4

--- setup.py
+++ setup.py
@@ -97,6 +97,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/oceanprotocol/squid-py',
-    version='0.5.3',
+    version='0.5.4',
     zip_safe=False,
 )
