3cdbc56f9dd4f54df877f33564b7c56716ef54c1
ssallam <samer@oceanprotocol.com>
2019-07-05 06:50:18 +0200

    Remove web3 dependency (ocean-commons has web3 dependency).

--- setup.py
+++ setup.py
@@ -24,7 +24,6 @@ install_requirements = [
     'pyopenssl',
     'PyJWT',  # not jwt
     'PyYAML==4.2b4',
-    'web3==4.5.0',
     'ocean-commons==0.1.0',
     'ocean-secret-store-client==0.0.1',
     'requests==2.21.0',
